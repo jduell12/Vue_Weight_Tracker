6cba45053499495c1d9bac6ac072a8652ef716b6
Jessica <duellje@oregonstate.edu>
2021-03-23 21:01:08 -0400

    added state getters, actions and mutations

--- weight-tracker/src/store/modules/darkMode.js
+++ weight-tracker/src/store/modules/darkMode.js
@@ -2,11 +2,19 @@ const state = {
   darkMode: true,
 };
 
-const getters = {};
+const getters = {
+  checkMode: (state) => state.darkMode,
+};
 
-const actions = {};
+const actions = {
+  changeMode({ commit }) {
+    commit("changeMode");
+  },
+};
 
-const mutations = {};
+const mutations = {
+  changeMode: (state) => (state.darkMode = !state.darkMode),
+};
 
 export default {
   state,
